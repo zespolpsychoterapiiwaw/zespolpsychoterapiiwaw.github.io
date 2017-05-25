167d1f8012c5c7840f38c1bdbd25d526ec7b2340
zespolpsychoterapiiwaw <michkad@gazeta.pl>
2017-05-25 03:01:01 +0200

    Jesus.. now proper version with directives!

--- js/pageController.js
+++ js/pageController.js
@@ -1,92 +1,88 @@
-var app = angular.module('myApp',  ['ngRoute', 'ngAnimate']);
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
 // configure our routes
-    app.config(function($routeProvider) {
-        $routeProvider
+app.config(function ($routeProvider) {
+    $routeProvider
 
-            // route for the home page
-            .when('/', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
+        // route for the home page
+        .when('/', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
 
-            .when('/offer', {
-                templateUrl : 'pages/offer.html',
-                controller  : 'offerController'
-            })
+        .when('/offer', {
+            templateUrl: 'pages/offer.html',
+            controller: 'offerController'
+        })
 
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
 
-            // route for the contact page
-            .when('/psychotherapy', {
-                templateUrl : 'pages/psychotherapy.html',
-                controller  : 'psychotherapyController'
-            });
-    });
+        // route for the contact page
+        .when('/psychotherapy', {
+            templateUrl: 'pages/psychotherapy.html',
+            controller: 'psychotherapyController'
+        });
+});
 
-    // create the controller and inject Angular's $scope
-    app.controller('mainController', function($scope) {
-    });
+// create the controller and inject Angular's $scope
+app.controller('mainController', function ($scope) {
+});
 
-    app.controller('aboutController', function($scope) {
-$scope.initialize = function(){
-    /* google maps */
-google.maps.visualRefresh = true;
+app.controller('aboutController', function ($scope) {
+});
 
-$scope.maps = [];
+app.controller('psychotherapyController', function ($scope) {
+});
 
-function initializeAllMaps()
-{
-        initialize('map-canvas1','ul. Waryńskiego 9, Warszawa')
-        initialize('map-canvas2','ul. Puławska 103, Warszawa,')
-        initialize('map-canvas3','ul. Jasna 1, Warszawa,')
-}
+app.controller('offerController', function ($scope) {
+});
 
-function initialize(id,address) {
-	var geocoder = new google.maps.Geocoder();
-	var mapOptions = {
-    	zoom: 15,
-    	mapTypeId: google.maps.MapTypeId.ROADMAP,
-     	scrollwheel: false
-	};
-	var map = new google.maps.Map(document.getElementById(id),mapOptions);
+app.directive('mapCanvas', function ($timeout) {
+    return {
+        templateUrl: 'pages/mapcanvas.html',
+        restrict: 'E',
+        scope: true,
+        scope: {
+            address: '@',
+            selectormap: '@'
+        },
+        link: function (scope, element) {
+            $timeout(function () {
+    //DOM has finished rendering
 
-  	if (geocoder) {
-      geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-          map.setCenter(results[0].geometry.location);
+            var geocoder = new google.maps.Geocoder();
 
-            var marker = new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map,
-                title:address
-            });
-            $scope.maps.push(map);
-          } else {  
-          	alert("No results found");
-          }
+            var mapOptions = {
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                scrollwheel: false
+            };
+            var map = new google.maps.Map(document.getElementById(scope.selectormap), mapOptions);
+            if (geocoder) {
+                geocoder.geocode({ 'address': scope.address }, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+                            map.setCenter(results[0].geometry.location);
+
+                            var marker = new google.maps.Marker({
+                                position: results[0].geometry.location,
+                                map: map,
+                                title: scope.address
+                            });
+                        } else {
+                            alert("No results found");
+                        }
+                    }
+                });
+            }
+        });
         }
-      });
-	}
-}
-initializeAllMaps();
-/* end google maps *//* end google maps */}; 
-
-       google.maps.event.addDomListener(window, 'load', $scope.initialize);   
-
-    });
-
-    app.controller('psychotherapyController', function($scope) {
-    });
-
-    app.controller('offerController', function($scope) {
-    });
-
+    };
+});
 
 
